74b673cedca8b0a74b750b410053e3a52f0dcd07
Ixchel Sykes <silmosfri8195@gmail.com>
2026-07-17 09:42:27 -0600

    Add files via upload

---
+++ Media,Mediana y moda.py
@@ -0,0 +1,11 @@
+import pandas as pd
+# Leer el archivo
+datos = pd.read_csv("estudiantes.csv")
+
+variables = ["Asistencia", "Tareas", "Examen"]
+
+for variable in variables:
+    print("\nVariable:", variable)
+    print("Media:", datos[variable].mean())
+    print("Mediana:", datos[variable].median())
+    print("Moda:", datos[variable].mode()[0])
