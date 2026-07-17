02505ccbd9591dd2f4be17b08de81db1e0052539
Ixchel Sykes <silmosfri8195@gmail.com>
2026-07-17 10:47:21 -0600

    Add files via upload

---
+++ Ejercicio2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+datos = pd.read_csv("estudiantes.csv")
+variables = [
+    "Asistencia",
+    "Tareas",
+    "Examen",
+    "Calificacion_Final",
+    "Horas_Estudio"
+]
+for variable in variables:
+    print("\nVariable:", variable)
+
+    rango=datos[variable].max()-datos[variable].min()
+    varianza=datos[variable].var()
+    desviacion=datos[variable].std()
+
+    print("Rango:", rango)
+    print("Varianza:", varianza)
+    print("Desviación estándar:", desviacion)
